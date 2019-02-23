7215db36b64accb3479d738d81565b80d114339f
Lucas Arvelo <lucasarvelo@gmail.com>
2019-02-23 16:29:59 -0500

    First Commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import {
+  MdApp,
+  MdToolbar,
+  MdButton,
+  MdIcon,
+  MdTabs,
+  MdDrawer,
+  MdList,
+  MdCard
+} from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
 
-Vue.config.productionTip = false
+Vue.use(MdApp);
+Vue.use(MdToolbar);
+Vue.use(MdButton);
+Vue.use(MdIcon);
+Vue.use(MdTabs);
+Vue.use(MdDrawer);
+Vue.use(MdList);
+Vue.use(MdCard);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount("#app");
