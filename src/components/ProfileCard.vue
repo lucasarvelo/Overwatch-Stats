882143efe0ea47b5efc8be68485cbae1be72a91b
Lucas Arvelo <lucasarvelo@gmail.com>
2019-03-03 18:03:05 -0500

    Profile Card

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,61 @@
+<template>
+  <md-card>
+    <md-card-media-cover md-solid>
+      <md-card-media>
+        <img v-bind:src="profileIcon">
+      </md-card-media>
+      <md-card-area>
+        <md-card-header>
+          <span class="md-title">{{profileName}}</span>
+          <span class="md-subhead">{{profileRating}}</span>
+        </md-card-header>
+
+        <md-card-actions>
+          <md-button class="md-icon-button">
+            <md-icon>favorite</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button">
+            <md-icon>share</md-icon>
+          </md-button>
+        </md-card-actions>
+      </md-card-area>
+    </md-card-media-cover>
+  </md-card>
+</template>
+
+<script>
+export default {
+  props: {
+    profileIcon: {
+      type: String,
+      required: true,
+      default: "@/assets/default-players-logo.png"
+    },
+    profileName: {
+      type: String,
+      required: true,
+      default: "Player Name"
+    },
+    profileRating: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    profileRatingIcon: {
+      type: String,
+      requiered: true,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style scoped>
+.md-card {
+  width: 320px;
+  margin: 4px;
+  display: inline-block;
+  vertical-align: top;
+}
+</style>
