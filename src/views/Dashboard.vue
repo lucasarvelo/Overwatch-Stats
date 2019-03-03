7641e0b0a09cf7e1b2fd7738fb038bd9b414d726
Lucas Arvelo <lucasarvelo@gmail.com>
2019-03-03 18:09:53 -0500

    Added ProfileCard to Dashboard view

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="dashboard"></div>
+  <div class="dashboard">
+    <ProfileCard
+      :profileIcon="this.profile.data.icon"
+      :profileName="this.profile.data.name"
+      :profileRating="this.profile.data.rating"
+      :profileRatingIcon="this.profile.data.ratingIcon"
+    />
+  </div>
 </template>
 
+<script>
+import ProfileCard from "@/components/ProfileCard.vue";
 
+export default {
+  name: "Dashboard",
+  components: {
+    ProfileCard
+  },
+  props: {
+    profile: {
+      type: Object,
+      default: () => {
+        return { data: {} };
+      }
+    }
+  }
+};
+</script>
