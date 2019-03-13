053b2393c96d36824f93c555e2032cf28b2c0da3
Lucas Arvelo <lucasarvelo@gmail.com>
2019-03-13 17:52:38 -0400

    Change API for profile

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,15 +1,13 @@
 const profileService = client => {
   return {
     get(battleTag) {
-      return new Promise(resolve => {
-        client
-          .get("https://ow-api.com/v1/stats/pc/us/" + battleTag + "/complete")
-          .then(profile => {
-            localStorage.setItem("profile", JSON.stringify(profile.data));
-            resolve(profile.data);
-          })
-          .catch(error => error);
-      });
+      return client
+        .get("http://overwatchy.com/profile/pc/us/" + battleTag)
+        .then(profile => {
+          localStorage.setItem("profile", JSON.stringify(profile.data));
+          return profile.data;
+        })
+        .catch(error => error);
     }
   };
 };
