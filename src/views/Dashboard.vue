3d53680907c7c45d8b4bff53e8c90ecb8d8ec88d
Lucas Arvelo <lucasarvelo@gmail.com>
2019-03-18 22:02:47 -0400

    Added stats to dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,16 +1,26 @@
 <template>
-  <div class="dashboard">
-    <ProfileCard v-if="this.$store.state.profile"/>
+  <div class="md-layout md-gutter">
+    <div
+      class="md-layout md-layout-item md-alignment-center-center md-large-size-15 md-medium-size-20 md-small-size-30 md-xsmall-size-100"
+    >
+      <ProfileCard v-if="this.$store.state.profile"/>
+    </div>
+    <div
+      class="md-layout md-layout-item md-large-size-85 md-medium-size-80 md-small-size-70 md-xsmall-size-100 md-alignment-center-center"
+    >
+      <StatsDashboard v-if="this.$store.state.profile && this.$store.state.stats"/>
+    </div>
   </div>
 </template>
 
 <script>
 import ProfileCard from "@/components/ProfileCard.vue";
-
+import StatsDashboard from "@/components/StatsDashboard.vue";
 export default {
   name: "Dashboard",
   components: {
-    ProfileCard
+    ProfileCard,
+    StatsDashboard
   },
   created() {
     this.$store.dispatch("fetchProfile");
@@ -19,3 +29,6 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+</style>
