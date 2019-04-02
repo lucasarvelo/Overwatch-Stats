7bc392ade596eb0cd61410b21d8be346455f0895
Lucas Arvelo <lucasarvelo@gmail.com>
2019-04-02 17:54:44 -0400

    Added Preloader

---
+++ src/components/Preloader/Preloader.vue
@@ -0,0 +1,17 @@
+<template>
+  <md-progress-spinner
+    :md-diameter="100"
+    :md-stroke="10"
+    md-mode="indeterminate"
+    class="center-align"
+  ></md-progress-spinner>
+</template>
+
+<script>
+export default {
+  name: "Preloader"
+};
+</script>
+
+<style scoped>
+</style>
