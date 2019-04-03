742c8b80e7436ed18b93ce1aaa0f35e8559fa777
Lucas Arvelo <lucasarvelo@gmail.com>
2019-04-02 21:21:06 -0400

    Added StatsCars

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,29 +1,35 @@
 <template>
-  <div class="md-layout md-gutter">
-    <div
-      class="md-layout md-layout-item md-alignment-center-center md-large-size-15 md-medium-size-20 md-small-size-30 md-xsmall-size-100"
-    >
-      <ProfileCard v-if="this.$store.state.profile"/>
-      <Preloader v-else/>
-    </div>
-    <div
-      class="md-layout md-layout-item md-large-size-85 md-medium-size-80 md-small-size-70 md-xsmall-size-100 md-alignment-center-center"
-    >
-      <StatsDashboard v-if="this.$store.state.profile && this.$store.state.stats"/>
+  <div
+    v-if="this.$store.state.profile && this.$store.state.stats"
+    class="md-layout md-gutter md-alignment-center-center content"
+  >
+    <ProfileCard/>
+    <StatsDashboard/>
+    <div class="md-layout-item md-layout md-alignment-center-center md-gutter">
+      <StatsCard
+        v-for="item in this.$store.state.stats.stats.best.competitive"
+        :key="item.title"
+        :title="item.title"
+        :content="item.value"
+      />
     </div>
   </div>
+  <Preloader v-else/>
 </template>
 
 <script>
 import ProfileCard from "@/components/ProfileCard/ProfileCard.vue";
 import StatsDashboard from "@/components/StatsDashboard/StatsDashboard.vue";
 import Preloader from "@/components/Preloader/Preloader.vue";
+import StatsCard from "@/components/StatsCard/StatsCard.vue";
+
 export default {
   name: "Dashboard",
   components: {
     ProfileCard,
     StatsDashboard,
-    Preloader
+    Preloader,
+    StatsCard
   },
   created() {
     this.$store.dispatch("fetchProfile");
@@ -34,4 +40,7 @@ export default {
 </script>
 
 <style scoped>
+.content {
+  margin-bottom: 64px;
+}
 </style>
