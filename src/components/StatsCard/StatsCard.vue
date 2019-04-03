2420dffdce548b6f89bc7514ad1eff2f6897ce7c
Lucas Arvelo <lucasarvelo@gmail.com>
2019-04-02 21:20:18 -0400

    Added StatsCard Component

---
+++ src/components/StatsCard/StatsCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <md-card
+    md-elevation-10
+    class="md-layout-item md-large-size-30 md-medium-size-33 md-small-size-45 md-xsmall-size-100"
+  >
+    <md-card-area>
+      <md-card-header>
+        <span class="md-subhead">{{title}}</span>
+        <br>
+        <span class="md-title">{{content}}</span>
+      </md-card-header>
+    </md-card-area>
+  </md-card>
+</template>
+
+<script>
+export default {
+  name: "StatsCard",
+  props: {
+    title: String,
+    content: String
+  }
+};
+</script>
+
+<style scoped>
+.md-layout-item {
+  margin: 5px;
+}
+</style>
