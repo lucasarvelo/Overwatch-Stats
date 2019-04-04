8d7b1e67e9d62e5fced135e56395b5ddff464ecd
Lucas Arvelo <lucasarvelo@gmail.com>
2019-04-04 15:18:14 -0400

    Add title to cards section

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -6,6 +6,7 @@
     <ProfileCard/>
     <StatsDashboard/>
     <div class="md-layout-item md-layout md-alignment-center-center md-gutter">
+      <h1 class="md-layout-item md-size-80">Competitive - Best</h1>
       <StatsCard
         v-for="item in this.$store.state.stats.stats.best.competitive"
         :key="item.title"
