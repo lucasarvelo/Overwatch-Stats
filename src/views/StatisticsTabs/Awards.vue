d13fa30dd2cf66d7b82438403b4e643a923f52b3
Lucas Arvelo <lucasarvelo@gmail.com>
2019-04-06 23:29:54 -0400

    Added views for Tabs in statistics

---
+++ src/views/StatisticsTabs/Awards.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="md-layout md-alignment-center-center md-gutter md-size-100">
+    <StatsCard
+      v-for="item in this.$store.state.stats.stats.match_awards.competitive"
+      :key="item.title"
+      :title="item.title"
+      :content="item.value"
+    />
+  </div>
+</template>
+
+<script>
+import StatsCard from "@/components/StatsCard/StatsCard.vue";
+
+export default {
+  name: "Awards",
+  components: {
+    StatsCard
+  }
+};
+</script>
+
+<style scoped>
+</style>
