f317ac7d90c9a0a7cd47f4d1aff0a47a9e0080d6
Lucas Arvelo <lucasarvelo@gmail.com>
2020-02-09 18:21:23 -0500

    Fix statistics views

--- src/views/StatisticsTabs/Assists.vue
+++ src/views/StatisticsTabs/Assists.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="md-layout md-alignment-center-center md-gutter md-size-100">
     <StatsCard
-      v-for="item in this.$store.state.stats.stats.competitive.all.assists"
-      :key="item.title"
-      :title="item.title"
-      :content="item.value"
+      v-for="(item, title) in this.$store.state.stats.stats.competitive.all.assists"
+      :key="title"
+      :title="title.replace(/_/g, ' ').toUpperCase()"
+      :content="item.toString()"
     />
   </div>
 </template>
